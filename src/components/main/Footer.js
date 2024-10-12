22e9931ecc3ffa4e7c8d125c589094d69d3bceae
FonuaBruh <frizkoksslow@gmail.com>
2024-10-12 17:05:38 +0300

    add footer with navigation panel

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,16 +1,52 @@
 import React from "react";
+import Instagram from "../icons/instagram_icon.svg";
+import Pinterest from "../icons/pinterest_icon.svg";
+import Twitter from "../icons/twitter_icon.svg";
+import Vk from "../icons/vk_icon.svg";
 
 export default function Footer() {
     return (
         <footer>
             <div className="container">
-                <h1>Global Minecraft</h1>
-                <h2>База знаний для комфортной и веселой игры</h2>
-                <nav>
-                    <ul>
-                        <li><a>123</a></li>
-                    </ul>
-                </nav>
+                <div className="footer-nav">
+                    <div className="logo">
+                        <p>Global</p>
+                        <p>Minecraft</p>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="">Моды</a></li>
+                            <li><a href="">Наборы текстур</a></li>
+                            <li><a href="">Шейдеры</a></li>
+                            <li><a href="">Сервера</a></li>
+                        </ul>
+                    </nav>
+                    <div className='socials'>
+                        <p>Наши социальные сети</p>
+                        <ul className="social-icons">
+                            <li>
+                                <a href="#">
+                                    <img src={Instagram} alt="Instagram" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={Pinterest} alt="Pinterest" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={Twitter} alt="Twitter" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={Vk} alt="Vk" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
     );
