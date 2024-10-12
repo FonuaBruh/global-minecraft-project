cc8635a1ab149670671e7ec97a625cfb6e5871cb
FonuaBruh <frizkoksslow@gmail.com>
2024-10-12 17:21:31 +0300

    add content layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Header from './components/main/Header';
 import Footer from './components/main/Footer';
+import Mods from './components/pages/mods/Mods';
 
 function App() {
     return (
         <div>
             <Header />
+            <Mods />
             <Footer />
         </div>
 
---
+++ src/components/pages/mods/Mods.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Mods() {
+    return (
+        <div>
+            <h1>Mods</h1>
+        </div>
+    );
+}
